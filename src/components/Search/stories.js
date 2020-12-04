fdc9cfe86c653bc4a8372d673761627c9444e954
Robin Wieruch <hello@rwieruch.com>
2020-12-04 12:06:29 +0100

    palceholdr searcg filter stories

---
+++ src/components/Search/stories.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import {
+  Table,
+  Header,
+  HeaderRow,
+  Body,
+  Row,
+  HeaderCell,
+  Cell
+} from '@table';
+
+const list = [
+  { id: '1', name: 'Hello', stars: 24, count: 42, light: true },
+  { id: '2', name: 'There', stars: 42, count: 24, light: false },
+  { id: '3', name: 'Nice', stars: 111, count: 111, light: true },
+  { id: '4', name: 'To', stars: 122, count: 133, light: false },
+  { id: '5', name: 'Meet', stars: 133, count: 122, light: true },
+  { id: '6', name: 'You', stars: 155, count: 155, light: true },
+  {
+    id: '7',
+    name: 'And Welcome To This Table Folks',
+    stars: 155,
+    count: 155,
+    light: true
+  }
+];
+
+storiesOf('01. Features/ 07. Search WIP', module)
+  .addParameters({ component: Table })
+  .add('default', () => {
+    return (
+      <Table list={list}>
+        {tableList => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Name</HeaderCell>
+                <HeaderCell>Stars</HeaderCell>
+                <HeaderCell>Light</HeaderCell>
+                <HeaderCell>Count</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map(item => (
+                <Row key={item.id} item={item}>
+                  <Cell>{item.name}</Cell>
+                  <Cell>{item.stars}</Cell>
+                  <Cell>{item.light.toString()}</Cell>
+                  <Cell>{item.count}</Cell>
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    );
+  });
