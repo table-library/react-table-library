7b102505f0766634f1c27c987a497a452d602a8e
Robin Wieruch <hello@rwieruch.com>
2020-12-04 05:30:40 +0100

    refactor

---
+++ src/components/Resize/Resizer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Resizer = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  cursor: col-resize;
+  width: 3px;
+`;
