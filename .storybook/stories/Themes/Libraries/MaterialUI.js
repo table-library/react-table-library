e1d18afcbf11199f45a4668736b2c0ef52c81616
Robin Wieruch <hello@rwieruch.com>
2021-01-29 19:23:17 +0100

    themes

---
+++ .storybook/stories/Themes/Libraries/MaterialUI.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('04. Library Themes/ 01. Material UI', module)
+  // .addParameters({ component: Table })
+  .add('default', () => {
+    return <div>WIP</div>;
+  });
---
+++ .storybook/stories/Themes/Products/GoogleDrive.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('05. Product Themes/ 01. Google Drive', module)
+  // .addParameters({ component: Table })
+  .add('default', () => {
+    return <div>WIP</div>;
+  });
