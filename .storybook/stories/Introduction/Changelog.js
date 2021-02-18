68380150e39aa0d18657b58e7d5c06e7427d39be
Robin Wieruch <hello@rwieruch.com>
2021-02-18 16:44:58 +0100

    new stories

---
+++ .storybook/stories/Introduction/Changelog.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('00. Intro/ 05. Changelog', module).add('default', () => {
+  return <p>Changelog link to GitHub ....</p>;
+});
