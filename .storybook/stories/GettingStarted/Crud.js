c450df04025363b28d40691f46738a35da904c97
Robin Wieruch <hello@rwieruch.com>
2021-02-18 13:12:13 +0100

    crud

--- .storybook/stories/GettingStarted/Crud.js
+++ .storybook/stories/GettingStarted/Crud.js
@@ -15,7 +15,7 @@ import {
 
 import { nodes } from '../data';
 
-storiesOf('01. First Steps/ 04. CRUD (WIP)', module)
+storiesOf('01. First Steps/ 04. CRUD', module)
   .addParameters({ component: Table })
   .add('default', () => {
     const data = { nodes };
@@ -63,12 +63,212 @@ storiesOf('01. First Steps/ 04. CRUD (WIP)', module)
       </Table>
     );
   })
-  .add('create WIP', () => {
-    return <div>create item</div>;
+  .add('create', () => {
+    const [data, setData] = React.useState({ nodes });
+    const [value, setValue] = React.useState('');
+
+    const handleChange = event => {
+      setValue(event.target.value);
+    };
+
+    const handleSubmit = event => {
+      const id = Math.floor(Math.random() * (9990 - 0 + 1)) + 0;
+
+      setData(state => ({
+        ...state,
+        nodes: state.nodes.concat({
+          id,
+          name: value,
+          deadline: new Date(),
+          type: 'LEARN',
+          isComplete: false,
+          nodes: null
+        })
+      }));
+
+      event.preventDefault();
+    };
+
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={value} onChange={handleChange} />
+          <button type="submit">Create</button>
+        </form>
+
+        <Table data={data}>
+          {tableList => (
+            <>
+              <Header>
+                <HeaderRow>
+                  <HeaderCell>Task</HeaderCell>
+                  <HeaderCell>Deadline</HeaderCell>
+                  <HeaderCell>Type</HeaderCell>
+                  <HeaderCell>Complete</HeaderCell>
+                  <HeaderCell>Tasks</HeaderCell>
+                </HeaderRow>
+              </Header>
+
+              <Body>
+                {tableList.map(item => (
+                  <Row key={item.id} item={item}>
+                    {tableItem => (
+                      <React.Fragment key={tableItem.id}>
+                        <Cell>{tableItem.name}</Cell>
+                        <Cell>
+                          {tableItem.deadline.toLocaleDateString(
+                            'fr-CA',
+                            {
+                              year: 'numeric',
+                              month: '2-digit',
+                              day: '2-digit'
+                            }
+                          )}
+                        </Cell>
+                        <Cell>{tableItem.type}</Cell>
+                        <Cell>{tableItem.isComplete.toString()}</Cell>
+                        <Cell>{tableItem.nodes?.length}</Cell>
+                      </React.Fragment>
+                    )}
+                  </Row>
+                ))}
+              </Body>
+            </>
+          )}
+        </Table>
+      </>
+    );
   })
-  .add('delete WIP', () => {
-    return <div>delete item</div>;
+  .add('delete', () => {
+    const [data, setData] = React.useState({ nodes });
+
+    const handleRemove = id => {
+      setData(state => ({
+        ...state,
+        nodes: state.nodes.filter(node => node.id !== id)
+      }));
+    };
+
+    return (
+      <Table data={data}>
+        {tableList => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Task</HeaderCell>
+                <HeaderCell>Deadline</HeaderCell>
+                <HeaderCell>Type</HeaderCell>
+                <HeaderCell>Complete</HeaderCell>
+                <HeaderCell>Tasks</HeaderCell>
+                <HeaderCell>Actions</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map(item => (
+                <Row key={item.id} item={item}>
+                  {tableItem => (
+                    <React.Fragment key={tableItem.id}>
+                      <Cell>{tableItem.name}</Cell>
+                      <Cell>
+                        {tableItem.deadline.toLocaleDateString(
+                          'fr-CA',
+                          {
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit'
+                          }
+                        )}
+                      </Cell>
+                      <Cell>{tableItem.type}</Cell>
+                      <Cell>{tableItem.isComplete.toString()}</Cell>
+                      <Cell>{tableItem.nodes?.length}</Cell>
+                      <Cell>
+                        <button
+                          type="button"
+                          onClick={() => handleRemove(tableItem.id)}
+                        >
+                          Remove
+                        </button>
+                      </Cell>
+                    </React.Fragment>
+                  )}
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    );
   })
-  .add('update WIP', () => {
-    return <div>update item</div>;
+  .add('update', () => {
+    const [data, setData] = React.useState({ nodes });
+
+    const handleUpdate = (value, id) => {
+      setData(state => ({
+        ...state,
+        nodes: state.nodes.map(node => {
+          if (node.id === id) {
+            return { ...node, name: value };
+          } else {
+            return node;
+          }
+        })
+      }));
+    };
+
+    return (
+      <Table data={data}>
+        {tableList => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Task</HeaderCell>
+                <HeaderCell>Deadline</HeaderCell>
+                <HeaderCell>Type</HeaderCell>
+                <HeaderCell>Complete</HeaderCell>
+                <HeaderCell>Tasks</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map(item => (
+                <Row key={item.id} item={item}>
+                  {tableItem => (
+                    <React.Fragment key={tableItem.id}>
+                      <Cell>
+                        <input
+                          style={{ width: '100%' }}
+                          type="text"
+                          value={tableItem.name}
+                          onChange={event =>
+                            handleUpdate(
+                              event.target.value,
+                              tableItem.id
+                            )
+                          }
+                        />
+                      </Cell>
+                      <Cell>
+                        {tableItem.deadline.toLocaleDateString(
+                          'fr-CA',
+                          {
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit'
+                          }
+                        )}
+                      </Cell>
+                      <Cell>{tableItem.type}</Cell>
+                      <Cell>{tableItem.isComplete.toString()}</Cell>
+                      <Cell>{tableItem.nodes?.length}</Cell>
+                    </React.Fragment>
+                  )}
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    );
   });
