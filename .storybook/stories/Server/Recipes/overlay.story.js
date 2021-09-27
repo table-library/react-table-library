6324ae3bceb5c810a11f42e221c9d817ad6425d9
Robin Wieruch <hello@rwieruch.com>
2021-09-27 15:49:32 +0200

    story(Overlay) new stpory

--- .storybook/stories/Server/Recipes/overlay.story.js
+++ .storybook/stories/Server/Recipes/overlay.story.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
+import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import {
   useCustom,
@@ -16,17 +18,54 @@ import {
 
 import { getData } from '../../server';
 
-storiesOf('Server Recipes/Overlay (WIP)', module)
+export const Absolute = styled.div`
+  position: absolute;
+
+  z-index: 5;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const BlurryOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  top: 0;
+  left: 0;
+
+  background: #ffffff;
+  filter: opacity(0.8);
+
+  z-index: 10;
+`;
+
+const OverlayLoading = () => (
+  <>
+    <BlurryOverlay />
+    <Absolute>
+      <CircularProgress />
+    </Absolute>
+  </>
+);
+
+storiesOf('Server Recipes/Overlay', module)
   .addParameters({ component: Table })
   .add('base', () => {
     const [data, setData] = React.useState({
       nodes: [],
     });
 
+    const [isLoading, setLoading] = React.useState(false);
+
     // initial fetching
 
     const doGet = React.useCallback(async (params) => {
+      setLoading(true);
       setData(await getData(params));
+      setLoading(false);
     }, []);
 
     React.useEffect(() => {
@@ -66,39 +105,43 @@ storiesOf('Server Recipes/Overlay (WIP)', module)
           />
         </label>
 
-        <Table data={data}>
-          {(tableList) => (
-            <>
-              <Header>
-                <HeaderRow>
-                  <HeaderCell>Task</HeaderCell>
-                  <HeaderCell>Deadline</HeaderCell>
-                  <HeaderCell>Type</HeaderCell>
-                  <HeaderCell>Complete</HeaderCell>
-                  <HeaderCell>Tasks</HeaderCell>
-                </HeaderRow>
-              </Header>
+        <div>
+          {isLoading && <OverlayLoading />}
 
-              <Body>
-                {tableList.map((item) => (
-                  <Row key={item.id} item={item}>
-                    <Cell>{item.name}</Cell>
-                    <Cell>
-                      {item.deadline.toLocaleDateString('en-US', {
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit',
-                      })}
-                    </Cell>
-                    <Cell>{item.type}</Cell>
-                    <Cell>{item.isComplete.toString()}</Cell>
-                    <Cell>{item.nodes?.length}</Cell>
-                  </Row>
-                ))}
-              </Body>
-            </>
-          )}
-        </Table>
+          <Table data={data}>
+            {(tableList) => (
+              <>
+                <Header>
+                  <HeaderRow>
+                    <HeaderCell>Task</HeaderCell>
+                    <HeaderCell>Deadline</HeaderCell>
+                    <HeaderCell>Type</HeaderCell>
+                    <HeaderCell>Complete</HeaderCell>
+                    <HeaderCell>Tasks</HeaderCell>
+                  </HeaderRow>
+                </Header>
+
+                <Body>
+                  {tableList.map((item) => (
+                    <Row key={item.id} item={item}>
+                      <Cell>{item.name}</Cell>
+                      <Cell>
+                        {item.deadline.toLocaleDateString('en-US', {
+                          year: 'numeric',
+                          month: '2-digit',
+                          day: '2-digit',
+                        })}
+                      </Cell>
+                      <Cell>{item.type}</Cell>
+                      <Cell>{item.isComplete.toString()}</Cell>
+                      <Cell>{item.nodes?.length}</Cell>
+                    </Row>
+                  ))}
+                </Body>
+              </>
+            )}
+          </Table>
+        </div>
       </>
     );
   });
