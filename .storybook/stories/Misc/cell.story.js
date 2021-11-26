ad7fe5e1201c1a2108b4c6f6e8e11f28298bfb1b
Robin Wieruch <hello@rwieruch.com>
2021-11-26 22:22:00 +0100

    cell stories

--- .storybook/stories/Misc/cell.story.js
+++ .storybook/stories/Misc/cell.story.js
@@ -12,6 +12,7 @@ import {
   HeaderCell,
   Cell,
 } from '@table-library/react-table-library/table';
+import { useTheme } from '@table-library/react-table-library/theme';
 
 import { nodes } from '../data';
 
@@ -63,6 +64,139 @@ storiesOf('Misc/Cell', module)
                   <Cell>{item.type}</Cell>
                   <Cell>{item.isComplete.toString()}</Cell>
                   <Cell>{item.nodes?.length}</Cell>
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    );
+  })
+  .add('cell outline', () => {
+    const data = { nodes };
+
+    const theme = useTheme({
+      BaseCell: `
+        &:focus {
+          outline: dotted;
+          outline-width: 1px;
+          outline-offset: -1px;
+        }
+      `,
+    });
+
+    return (
+      <Table data={data} theme={theme}>
+        {(tableList) => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Task</HeaderCell>
+                <HeaderCell>Deadline</HeaderCell>
+                <HeaderCell>Type</HeaderCell>
+                <HeaderCell>Complete</HeaderCell>
+                <HeaderCell>Tasks</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map((item, index) => (
+                <Row key={item.id} item={item}>
+                  <Cell
+                    tabIndex="-1"
+                    onClick={(event) =>
+                      console.log('Click Cell', event)
+                    }
+                  >
+                    {item.name}
+                  </Cell>
+                  <Cell tabIndex="-1">
+                    {item.deadline.toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: '2-digit',
+                      day: '2-digit',
+                    })}
+                  </Cell>
+                  <Cell tabIndex="-1">{item.type}</Cell>
+                  <Cell tabIndex="-1">
+                    {item.isComplete.toString()}
+                  </Cell>
+                  <Cell tabIndex="-1">{item.nodes?.length}</Cell>
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    );
+  })
+  .add('cell tabbable', () => {
+    const data = { nodes };
+
+    const theme = useTheme({
+      BaseCell: `
+        &:focus {
+          outline: dotted;
+          outline-width: 1px;
+          outline-offset: -1px;
+        }
+      `,
+    });
+
+    return (
+      <Table data={data} theme={theme}>
+        {(tableList) => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Task</HeaderCell>
+                <HeaderCell>Deadline</HeaderCell>
+                <HeaderCell>Type</HeaderCell>
+                <HeaderCell>Complete</HeaderCell>
+                <HeaderCell>Tasks</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map((item, index) => (
+                <Row key={item.id} item={item}>
+                  <Cell
+                    ariaColindex={0 + index * 5}
+                    tabIndex={0 + index * 5}
+                    onClick={(event) =>
+                      console.log('Click Cell', event)
+                    }
+                  >
+                    {item.name}
+                  </Cell>
+                  <Cell
+                    ariaColindex={1 + index * 5}
+                    tabIndex={1 + index * 5}
+                  >
+                    {item.deadline.toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: '2-digit',
+                      day: '2-digit',
+                    })}
+                  </Cell>
+                  <Cell
+                    ariaColindex={2 + index * 5}
+                    tabIndex={2 + index * 5}
+                  >
+                    {item.type}
+                  </Cell>
+                  <Cell
+                    ariaColindex={3 + index * 5}
+                    tabIndex={3 + index * 5}
+                  >
+                    {item.isComplete.toString()}
+                  </Cell>
+                  <Cell
+                    ariaColindex={4 + index * 5}
+                    tabIndex={4 + index * 5}
+                  >
+                    {item.nodes?.length}
+                  </Cell>
                 </Row>
               ))}
             </Body>
