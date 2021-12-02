8d86e8935dbd692d27d4831c4fdc8232ed122e8f
Robin Wieruch <hello@rwieruch.com>
2021-12-02 11:30:54 +0100

    fix(Resize) move resizer to left, so that it does not cause overflow horizontally

--- src/resize/styles.js
+++ src/resize/styles.js
@@ -6,7 +6,7 @@ const resizerStyle = (resize) => {
     z-index: 1;
     position: absolute;
     top: 0;
-    right: ${-width / 2}px;
+    right: 0;
     bottom: 0;
     cursor: col-resize;
     width: ${width}px;
