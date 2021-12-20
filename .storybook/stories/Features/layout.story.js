52385237b7113c044546a69a6c7d05681cf19898
Robin Wieruch <hello@rwieruch.com>
2021-12-20 10:20:21 +0100

    story(layou) evenly distributed

--- .storybook/stories/Features/layout.story.js
+++ .storybook/stories/Features/layout.story.js
@@ -18,6 +18,45 @@ import { nodes } from '../data';
 
 storiesOf('Features/Layout', module)
   .addParameters({ component: Table })
+  .add('evenly distributed (default)', () => {
+    const data = { nodes };
+
+    return (
+      <Table data={data}>
+        {(tableList) => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Task</HeaderCell>
+                <HeaderCell>Deadline</HeaderCell>
+                <HeaderCell>Type</HeaderCell>
+                <HeaderCell>Complete</HeaderCell>
+                <HeaderCell>Tasks</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map((item) => (
+                <Row key={item.id} item={item}>
+                  <Cell>{item.name}</Cell>
+                  <Cell>
+                    {item.deadline.toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: '2-digit',
+                      day: '2-digit',
+                    })}
+                  </Cell>
+                  <Cell>{item.type}</Cell>
+                  <Cell>{item.isComplete.toString()}</Cell>
+                  <Cell>{item.nodes?.length}</Cell>
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    );
+  })
   .add('nth-child', () => {
     const theme = useTheme({
       BaseCell: `
