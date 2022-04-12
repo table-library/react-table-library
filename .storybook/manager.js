9711166a5f91fcc2237a5bf986eb3dd7da282a11
Robin Wieruch <hello@rwieruch.com>
2022-04-12 14:00:26 +0200

    docs(storybook/main): remove version

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,11 +1,9 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming';
 
-import { version } from '../package.json';
-
 addons.setConfig({
   theme: create({
-    brandTitle: `React Table Library (v${version})`,
+    brandTitle: 'React Table Library',
     brandUrl: 'https://github.com/table-library/react-table-library',
   }),
   showPanel: false,
