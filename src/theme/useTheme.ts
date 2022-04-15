5c1aabbd7ece2a3590b125cf5526da190345792f
Robin Wieruch <hello@rwieruch.com>
2022-04-15 23:51:03 +0200

    feat(useTheme): merge Base* with *

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -1,15 +1,16 @@
 import { Theme } from '@table-library/react-table-library/types/theme';
+import { css } from '@emotion/react';
 
-const zipThemes = (themes: Theme[]): Theme =>
-  themes.reduce((acc: Record<string, any>, value: any) => {
+const zipThemes = (themes: Theme[]): Record<string, any> => {
+  const zippedThemes = themes.reduce((acc: Record<string, any>, value: any) => {
     const keys = Object.keys(value);
 
     keys.forEach((key) => {
       if (!acc[key]) {
-        acc[key] = '';
+        acc[key] = css``;
       }
 
-      acc[key] = `
+      acc[key] = css`
         ${acc[key]}
         ${value[key]}
       `;
@@ -17,6 +18,44 @@ const zipThemes = (themes: Theme[]): Theme =>
 
     return acc;
   }, {});
+
+  const baseCell = zippedThemes.BaseCell || '';
+  const headerCell = zippedThemes.HeaderCell || '';
+  const cell = zippedThemes.Cell || '';
+
+  const baseRow = zippedThemes.BaseRow || '';
+  const headerRow = zippedThemes.HeaderRow || '';
+  const row = zippedThemes.Row || '';
+
+  const mergedBasesThemes = {
+    ...zippedThemes,
+    HeaderRow: css`
+      ${baseRow}
+      ${headerRow}
+    `,
+    Row: css`
+      ${baseRow}
+      ${row}
+    `,
+    HeaderCell: css`
+      ${baseCell}
+      ${headerCell}
+    `,
+    Cell: css`
+      ${baseCell}
+      ${cell}
+    `,
+  };
+
+  return Object.keys(mergedBasesThemes).reduce((acc: Record<string, any>, key: any) => {
+    acc[key] = {
+      // @ts-ignore
+      css: mergedBasesThemes[key],
+    };
+
+    return acc;
+  }, {});
+};
 
 const useTheme = (theme: Theme | Theme[]): Theme => {
   if (Array.isArray(theme)) {
