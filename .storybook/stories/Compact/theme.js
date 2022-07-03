285ffd3672d2f1d17fd5d3d72f21e7a7203fe0f0
Robin Wieruch <hello@rwieruch.com>
2022-07-03 12:57:24 +0200

    docs(Compact): custom theme should use baseline theme

--- .storybook/stories/Compact/theme.js
+++ .storybook/stories/Compact/theme.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { CompactTable } from '@table-library/react-table-library/compact';
 import { useTheme } from '@table-library/react-table-library/theme';
+import { getTheme } from '@table-library/react-table-library/baseline';
 
 import { DocumentationSee } from '../documentation';
 import { nodes } from '../data';
@@ -11,20 +12,23 @@ const key = 'Theme';
 const Component = () => {
   const data = { nodes };
 
-  const theme = useTheme({
-    HeaderRow: `
-      background-color: #eaf5fd;
-    `,
-    Row: `
-      &:nth-of-type(odd) {
-        background-color: #d2e9fb;
-      }
-
-      &:nth-of-type(even) {
+  const theme = useTheme([
+    getTheme(),
+    {
+      HeaderRow: `
         background-color: #eaf5fd;
-      }
-    `,
-  });
+      `,
+      Row: `
+        &:nth-of-type(odd) {
+          background-color: #d2e9fb;
+        }
+
+        &:nth-of-type(even) {
+          background-color: #eaf5fd;
+        }
+      `,
+    },
+  ]);
 
   const COLUMNS = [
     { label: 'Task', renderCell: (item) => item.name },
