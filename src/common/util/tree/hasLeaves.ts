d82d04994cd198f4f41f52312d2a32ee24323814
Robin Wieruch <hello@rwieruch.com>
2022-08-31 14:17:25 +0200

    fix(hasLeaves): allow null

--- src/common/util/tree/hasLeaves.ts
+++ src/common/util/tree/hasLeaves.ts
@@ -1,3 +1,4 @@
+import { Nullish } from '@table-library/react-table-library/types/common';
 import { TableNode } from '@table-library/react-table-library/types/table';
 
-export const hasLeaves = <T extends TableNode>(node?: T): boolean => !!node?.nodes?.length;
+export const hasLeaves = <T extends TableNode>(node: T | Nullish): boolean => !!node?.nodes?.length;
