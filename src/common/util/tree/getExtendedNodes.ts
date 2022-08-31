16fee622ed25d52ce44b591615cc9b3e9349b7e8
Robin Wieruch <hello@rwieruch.com>
2022-08-31 13:58:04 +0200

    fix(getExtendedNodes): parentNode is null in first iteration

--- src/common/util/tree/getExtendedNodes.ts
+++ src/common/util/tree/getExtendedNodes.ts
@@ -1,6 +1,6 @@
 import { TableNode } from '@table-library/react-table-library/types/table';
 
-export const getExtendedNodes = (nodes: TableNode[], parentNode: TableNode): TableNode[] => {
+export const getExtendedNodes = (nodes: TableNode[], parentNode: TableNode | null): TableNode[] => {
   const extendedNodes = nodes.map((node) => ({
     ...node,
     ancestors: parentNode ? parentNode.ancestors.concat(node) : [node],
