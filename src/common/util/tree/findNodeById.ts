70961e68cacd4c496f8932066de1c20847f2f7b4
Robin Wieruch <hello@rwieruch.com>
2022-10-06 12:10:27 +0200

    fix(findNodeById): allow number

--- src/common/util/tree/findNodeById.ts
+++ src/common/util/tree/findNodeById.ts
@@ -1,6 +1,6 @@
 import { TableNode } from '@table-library/react-table-library/types/table';
 
-export const findNodeById = <T extends TableNode>(nodes: T[], id: string): T | null =>
+export const findNodeById = <T extends TableNode>(nodes: T[], id: string | number): T | null =>
   nodes.reduce((acc: T | null, value: T): T | null => {
     if (acc) return acc;
 
